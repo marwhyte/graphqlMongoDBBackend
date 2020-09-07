0eed5b3481309b283e20542f55dae38b84c67395
Marco Whyte <mwhyte@umass.edu>
2020-09-07 17:52:30 -0500

    express app apolloserver

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import { ApolloServer, gql } from "apollo-server-express";
+import { typeDefs, resolvers } from "./schema";
 import "babel-polyfill";
 import mongoose from "mongoose";
 import express from "express";
 import cookieParser from "cookie-parser";
 import "reflect-metadata";
 
-console.log("hi");
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 5000 }, () =>
+  console.log(`server running at http://localhost:4000${server.graphqlPath}`)
+);
