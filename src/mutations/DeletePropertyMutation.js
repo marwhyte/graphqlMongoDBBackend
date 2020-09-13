1473e9305d098ef10a6603f0cc34aa620cbaea6c
Marco Whyte <mwhyte@umass.edu>
2020-09-12 20:39:12 -0500

    can delete

--- src/mutations/DeletePropertyMutation.js
+++ src/mutations/DeletePropertyMutation.js
@@ -2,6 +2,8 @@ import { GraphQLString, GraphQLBoolean, GraphQLInt, GraphQLID } from "graphql";
 import PropertyClass from "../propertyActions";
 import { propertyType } from "../types/nodeInterfaceType";
 import UserClass from "../userActions";
+import { ObjectId } from "mongodb";
+
 export const DeleteProperty = {
   type: GraphQLBoolean,
   args: {
@@ -9,12 +11,18 @@ export const DeleteProperty = {
     userID: { type: GraphQLString },
   },
   resolve: async (_, { _id, userID }) => {
+    // compare
+
     const propertyClass = new PropertyClass();
     const userClass = new UserClass();
 
+    const propertyCreator = await propertyClass.getPropertyById(_id);
     const currUser = await userClass.getUserById(userID);
-    console.log(currUser);
-    if (currUser && currUser._id === userID) {
+
+    console.log("property", propertyCreator.createdBy);
+    console.log("user", currUser.username);
+
+    if (propertyCreator.createdBy === currUser.username) {
       const res = await propertyClass.deleteProperty(_id);
       if (res.value) {
         return true;
